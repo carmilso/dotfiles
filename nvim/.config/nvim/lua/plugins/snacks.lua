d3eb9722a26fcb60e3d5295d2e4dd7074c7e9f23
Carlos Millán Soler <cmillan@sciling.com>
2025-11-10 13:40:08 +0100

    feat(nvim): Add Avante Neovim config subproject and snacks.nvim plugin

---
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "folke/snacks.nvim",
+    opts = {
+      scope = {
+        enabled = true, -- enable tab-local buffer scoping
+        -- You can add more options here if needed
+      },
+    },
+  },
+}
